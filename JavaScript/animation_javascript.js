02a718baceec913b62f812e15a8cc576140a406c
johorin <hoyuak2145@docomo.ne.jp>
2020-11-28 15:28:57 +0900

    いろいろ追加、修正しました。

---
+++ JavaScript/animation_javascript.js
@@ -0,0 +1,11 @@
+const fadeInImage = document.getElementById('fadeInImage');
+fadeInImage.animate([{opacity: '0'}, {opacity: '1'}], 1500);
+
+//const fadeInIcon = document.getElementById('fadeInIcon');
+//fadeInIcon.animate([{opacity: '0'}, {opacity: '1'}], 1500);
+
+/*var fadein = function() {
+    fadeInIcon.animate([{opacity: '0'}, {opacity: '1'}], 1500);
+}
+
+setInterval(fadein(), 1000);*/
---
+++ jQuery/repeat_jquery.js
@@ -0,0 +1,40 @@
+/*(function($) {
+    $(function() {
+        let $element = $("#fadeInIcon");
+        $element.hide();
+        setInterval(function() {
+            $element.fadeIn(500);
+        }, 100);
+    });    
+})(jQuery);*/
+
+/*(function($) {
+    let $element = $("#fadeInIcon");
+    $element.hide();
+    $element.fadeIn(500, function(){ $(this).hide(); });
+})(jQuery);*/
+
+(function($) {
+    $(function() {
+        let $element = $("#fadeInIcon");
+        setInterval(function() {
+            $element.fadeOut(700, function(){
+                $element.fadeIn(700);
+            });
+        }, 100);
+    });    
+})(jQuery);
+
+/*(function($) {
+    $(function() {
+        setInterval(function() {
+            $element.animate({
+                top: 20
+            }, 700, function() {
+                $element.animate({
+                    bottom: 20
+                }, 700);
+            });
+        }, 100);
+    });
+})(iQuery);*/
